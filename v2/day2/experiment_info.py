92b6a4f8cd3ec95605558946a32a3d47d10a0351
Sangeetha Nandakumar <98574336+sangeethankumar@users.noreply.github.com>
2024-10-11 11:56:49 +0200

    section 3

---
+++ v2/day2/experiment_info.py
@@ -0,0 +1,4 @@
+experiment_name = "Mice Visual Cortex"
+num_mice = 25
+num_neuropixels = 300
+num_electrodes = 100
---
+++ v2/day2/run_section/hello.py
@@ -0,0 +1,7 @@
+print("Hello world")
+name='John Doe'
+age=100
+
+
+def add_two_numbers(a, b):
+    return a+b
